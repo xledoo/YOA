fdb3cd312ec8ce17e6976428bb0081e69f10b6bb
xledoo <xledoo@qq.com>
2014-09-12 17:53:02 +0800

    Add 现金融资管理

--- Application/Home/Controller/FinancingController.class.php
+++ Application/Home/Controller/FinancingController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 use Common\Controller\BaseController;
 class FinancingController extends BaseController {
     public function fcash(){
+    	$fc = M('finance_cash')->select();
+    	$this->assign('fcash',$fc);
     	$this->display();
     }
 
@@ -11,6 +13,23 @@ class FinancingController extends BaseController {
     }
 
     public function borrow(){
+    	$this->display();
+    }
+
+    public function add($stype){
+    	if(IS_POST){
+    		zecho($_POST);
+    	} else {
+    		$this->display();
+    	}
+    }
+
+    public function cashinfo($id){
+    	$info	=	M('finance_cash')->where("id=%d", $id)->find();
+    	$ralog = M('finance_ratelog')->where("mobile='%s' AND customer='%s'", array($info['mobile'], $info['customer']))->select();
+    	// zecho($ralog);
+    	$this->assign('cash', $info);
+    	$this->assign('cashlog', $ralog);
     	$this->display();
     }
 }
