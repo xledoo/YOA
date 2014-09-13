6e8c9a4b35746f4e285c864134a3e07b61c27ce1
alipiapia <124910168@qq.com>
2014-09-13 16:50:42 +0800

    Update View/Financing

--- Application/Home/Controller/FinancingController.class.php
+++ Application/Home/Controller/FinancingController.class.php
@@ -2,28 +2,35 @@
 namespace Home\Controller;
 use Common\Controller\BaseController;
 class FinancingController extends BaseController {
+
+    //现金融资
     public function fcash(){
     	$fc = M('finance_cash')->select();
     	$this->assign('fcash',$fc);
     	$this->display();
     }
 
+    //信用卡融资
     public function fCard(){
     	$this->display();
     }
 
+    //资金拆借
     public function borrow(){
     	$this->display();
     }
 
-    public function add($stype){
+    //添加现金融资业务
+    public function addcash($stype){
     	if(IS_POST){
-    		zecho($_POST);
+    		zecho($_REQUEST);
+            M('finance_cash')->add();
     	} else {
     		$this->display();
     	}
     }
 
+    //现金融资详情
     public function cashinfo($id){
     	$info	=	M('finance_cash')->where("id=%d", $id)->find();
     	$ralog = M('finance_ratelog')->where("mobile='%s' AND customer='%s'", array($info['mobile'], $info['customer']))->select();
