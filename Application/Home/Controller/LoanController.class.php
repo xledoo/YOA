b9a4260c62a5c103b402ac46cb430030ff6e8ba0
alipiapia <124910168@qq.com>
2014-10-20 23:56:55 +0800

    Update 房产抵押借贷

--- Application/Home/Controller/LoanController.class.php
+++ Application/Home/Controller/LoanController.class.php
@@ -3,10 +3,34 @@ namespace Home\Controller;
 use Common\Controller\BaseController;
 class LoanController extends BaseController {
     public function add(){
+    	if(IS_POST){
+    		// zecho($_POST);
+    		if($_POST['add_loan'] == 'housing'){
+    			$this->redirect('home/loan/add_housing');
+    		} elseif ($_POST['add_loan'] == 'car') {
+    			$this->redirect('home/loan/add_car');
+    		}
+    	} else {
+    		$this->display();
+    	}
+    }
+
+    public function llist(){
     	$this->display();
     }
 
-    public function help(){
+    public function add_to_housing(){
+    	if(IS_POST){
+    		zecho($_POST);
+    	}else{
+			$info = M('common_member_profile_setting')->select();
+	    	$this->assign('add_loan',$info);
+	    	$this->display();
+    	}
+    }
+
+    public function add_to_car(){
+    	zecho($_POST);
     	$this->display();
     }
 }
